35be0f9cb466e092699800ccb9491cdbb5b9e56e
nur-qushairi <menurqushairis@gmail.com>
2025-04-20 22:22:11 +0800

    Updates

---
+++ precache-manifest.a9d4081a6ea260c2e3f68f2d3107b436.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62da32d9e5f9daa4b7c5b7ce0265c9c",
+    "url": "/nurqushairi-portfolio/index.html"
+  },
+  {
+    "revision": "02ae9804cace5c6d068e",
+    "url": "/nurqushairi-portfolio/static/css/2.d660b8ca.chunk.css"
+  },
+  {
+    "revision": "0c7b4d02f08d134ed847",
+    "url": "/nurqushairi-portfolio/static/css/main.e7d2fab8.chunk.css"
+  },
+  {
+    "revision": "02ae9804cace5c6d068e",
+    "url": "/nurqushairi-portfolio/static/js/2.7a5b3105.chunk.js"
+  },
+  {
+    "revision": "47f7eb342e9ec131679f",
+    "url": "/nurqushairi-portfolio/static/js/3.0db04885.chunk.js"
+  },
+  {
+    "revision": "0c7b4d02f08d134ed847",
+    "url": "/nurqushairi-portfolio/static/js/main.69ce86d0.chunk.js"
+  },
+  {
+    "revision": "24032f703e4f0d5e255c",
+    "url": "/nurqushairi-portfolio/static/js/runtime~main.f0b144b8.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/nurqushairi-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/nurqushairi-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/nurqushairi-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "86a25f981ef29606d712adcfaad972cc",
+    "url": "/nurqushairi-portfolio/static/media/Qushairi.86a25f98.png"
+  },
+  {
+    "revision": "40fc696c5a861673b69a5bcb07e3fbba",
+    "url": "/nurqushairi-portfolio/static/media/coding_bg.40fc696c.jpg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/nurqushairi-portfolio/static/media/color-radiant.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/nurqushairi-portfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "cd03f287084273fb51464116830ac43e",
+    "url": "/nurqushairi-portfolio/static/media/contact-girl.cd03f287.png"
+  },
+  {
+    "revision": "d29b5b019916c70cf8496f3182f885f0",
+    "url": "/nurqushairi-portfolio/static/media/footer_bg.d29b5b01.png"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/nurqushairi-portfolio/static/media/github.d22ee372.png"
+  },
+  {
+    "revision": "651304581eb96e9e93bfe94cf882b583",
+    "url": "/nurqushairi-portfolio/static/media/laptop-pic.65130458.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/nurqushairi-portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "60fca85eaf7a5a4357a2960244b0650b",
+    "url": "/nurqushairi-portfolio/static/media/project-dongfeng.60fca85e.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/nurqushairi-portfolio/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "67b94c5f6ccccc68594d624a785b00e3",
+    "url": "/nurqushairi-portfolio/static/media/project-myportfolio.67b94c5f.png"
+  },
+  {
+    "revision": "256d3e62b5771fa3e078715ceae170bc",
+    "url": "/nurqushairi-portfolio/static/media/project-pbht.256d3e62.png"
+  },
+  {
+    "revision": "1803f881c475692cd791092ae4be3bcc",
+    "url": "/nurqushairi-portfolio/static/media/project-pbroadtax.1803f881.png"
+  },
+  {
+    "revision": "b7c24ed4c5e29f15c0f7eabebb09216f",
+    "url": "/nurqushairi-portfolio/static/media/project-pronet4u.b7c24ed4.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/nurqushairi-portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/nurqushairi-portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/nurqushairi-portfolio/static/media/revicons.2feb69cc.eot"
+  }
+]);
